cda936a13fc356c5270c7a6846af71dc0f2ff55f
jmazuelos <jmazuelos@uoc.edu>
2023-05-05 22:00:21 +0200

    Actualización a producción

---
+++ dist/index.aa69868b.js
@@ -0,0 +1,14 @@
+$(window).on("load", function() {
+    document.querySelector(".menu").style.display = "none";
+});
+const sideMenu = document.querySelector(".menu");
+const menuBtn = document.querySelector("#menuBtn");
+const closeBtn = document.querySelector("#closeBtn");
+document.querySelector("#menuBtn").addEventListener("click", ()=>{
+    sideMenu.style.display = "block";
+});
+document.querySelector("#closeBtn").addEventListener("click", ()=>{
+    sideMenu.style.display = "none";
+});
+
+//# sourceMappingURL=index.aa69868b.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,15 @@
-const name = "world"
-console.log('Hello ${name}');
+$( window ).on( "load", function() {
+    document.querySelector(".menu").style.display = 'none';
+} )
+
+const sideMenu = document.querySelector(".menu");
+const menuBtn = document.querySelector("#menuBtn");
+const closeBtn = document.querySelector("#closeBtn");
+
+document.querySelector("#menuBtn").addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+document.querySelector("#closeBtn").addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
