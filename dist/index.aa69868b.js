5d23b35f1ddb48b3100acdc4e3b36440d5f64ca3
jmazuelos <jmazuelos@uoc.edu>
2023-06-11 00:05:03 +0200

    Removed dist folder

--- dist/index.aa69868b.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("hamburger").addEventListener("click", function() {
-    document.getElementById("top").classList.toggle("top");
-    document.getElementById("middle").classList.toggle("middle");
-    document.getElementById("bottom").classList.toggle("bottom");
-    if ($("#menu").is(":visible")) $("#menu").hide();
-    else $("#menu").show();
-});
-
-//# sourceMappingURL=index.aa69868b.js.map
